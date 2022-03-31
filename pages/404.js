cfde4cecc9dd8bba8592addfbaf4cb2626cf3b80
AzureAD\KelvinLuz <esx.kluz@terceiros.mag.com.br>
2022-03-31 11:50:02 -0300

    first commit

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from "../src/components/Link";
+
+export default function Page404() {
+    return(
+        <div>
+            <h1>Opa, não encontrado :O</h1>
+            <Link href="/">
+                Ir para Home
+            </Link>
+        </div>
+    );
+}
